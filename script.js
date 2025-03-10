b6a4c49e750ce3bb0e2ed39469b9dd4b36327dc4
Ali Jan <alijandeveloper15@gmail.com>
2025-03-10 12:56:39 -0700

    get elements

---
+++ script.js
@@ -0,0 +1,7 @@
+const form = document.getElementById('signupForm');
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const phoneInput = document.getElementById('phone');
+const passwordInput = document.getElementById('password');
+const confirmPasswordInput = document.getElementById('confirmPassword');
+const successMessage = document.getElementById('successMessage');
